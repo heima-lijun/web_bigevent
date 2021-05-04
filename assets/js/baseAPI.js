6e46e6e638f4cf040234c0ff6d28f55b266cf7d6
itheima <ithei@itcast.cn>
2021-05-05 06:56:00 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,20 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     console.log(options.url);
+    //同意有权限的接口 
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 回调函数complete判断服务器数据
+    options.complete = function(res) {
+        if (res.responseJSON.message === '身份认证失败！' && res.responseJSON.status === 1) {
+            //清空token
+            localStorage.removeItem('token')
+                //跳转页面
+            location.href = '/login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getUserInfo()
+    var layer = layui.layer
+    $('#btnLogout').on('click', function() {
+        //eg1
+        layer.confirm('确认退出', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            //清空token
+            localStorage.removeItem('token')
+                //跳转页面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userInfo',
+        //headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status != 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+        //欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        //按需求渲染头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var frist = name[0].toUpperCase()
+        $('.text-avatar').html(frist).show()
+    }
+
+}
